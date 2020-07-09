89220ba14b86a2160f4e122e8e4ad35d49949f87
GarrettRathke <53503976+GarrettRathke@users.noreply.github.com>
2020-07-08 22:04:33 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const http = require('http');
-
-//create a server object:
-http.createServer(function (req, res) {
-  res.write('Hello World!'); //write a response to the client
-  res.end(); //end the response
-}).listen(8080); //the server object listens on port 8080
+    fs = require('fs');
+fs.readFile('/index.html', function (err, html) {
+    if (err) {
+        throw err; 
+    }       
+    http.createServer(function(request, response) {  
+        response.writeHeader(200, {"Content-Type": "text/html"});  
+        response.write(html);  
+        response.end();  
+    }).listen(80);
+});
