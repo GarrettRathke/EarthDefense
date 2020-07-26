4a135ad233aacbab9e72a245f9438b96e1e77283
Garrett Rathke <garrett.rathke@gmail.com>
2020-07-25 23:08:16 -0600

    Initial commit using Node in VS Code

---
+++ app.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var fs = require('fs');
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  fs.readFile(__dirname + 'index.html', function(error, data) {
+    if(error) {
+      res.writeHead(404);
+      res.write(error);
+      res.end();
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
